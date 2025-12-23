dc9bbbd2d3ea640b5dfb0c823ce2bd6bca0ea2e4
eduardo <real.777.em@gmail.com>
2025-12-23 08:14:47 -0500

    actualizando repositorio

--- peques/js/main.js
+++ peques/js/main.js
@@ -1,21 +1,65 @@
-// CLICK EN CARD
-document.querySelectorAll('.producto-card').forEach(card => {
-  card.addEventListener('click', () => {
-    const id = card.dataset.id;
-    window.location.href = `producto.html?id=${id}`;
-  });
+
+// Mostrar más productos en móvil
+
+const btn = document.getElementById("btnVerMas");
+const extras = document.querySelectorAll(".extra-mobile");
+
+btn.addEventListener("click", () => {
+
+  const ocultos = [...extras].filter(e => e.classList.contains("d-none"));
+
+  if (ocultos.length > 0) {
+    // Mostrar todos
+    extras.forEach(e => e.classList.remove("d-none"));
+    btn.innerText = "Ver menos";
+  } else {
+    // Ocultar y volver a mostrar solo 4
+    extras.forEach(e => e.classList.add("d-none"));
+    btn.innerText = "Ver más";
+    window.scrollTo({ top: btn.offsetTop - 200, behavior: "smooth" });
+  }
 });
 
-// CARGAR DETALLE
-if (window.location.pathname.includes("producto.html")) {
-  const id = new URLSearchParams(window.location.search).get("id");
-  const p = productos[id];
 
-  if (p) {
-    document.getElementById("nombre").textContent = p.nombre;
-    document.getElementById("precio").textContent = p.precio;
-    document.getElementById("descripcion").textContent = p.descripcion;
-    document.getElementById("imgProducto").src = p.img;
-    document.getElementById("btnWhatsapp").href = p.whatsapp;
+
+// mostrar lista 2
+
+const btn1 = document.getElementById("btnVerMass");
+const extras1 = document.querySelectorAll(".modo-mobile");
+
+btn1.addEventListener("click", () => {
+
+  const ocultos = [...extras1].filter(e => e.classList.contains("d-none"));
+
+  if (ocultos.length > 0) {
+    // Mostrar todos
+    extras1.forEach(e => e.classList.remove("d-none"));
+    btn1.innerText = "Ver menos";
+  } else {
+    // Ocultar y volver a mostrar solo 4
+    extras1.forEach(e => e.classList.add("d-none"));
+    btn1.innerText = "Ver más";
+    window.scrollTo({ top: btn1.offsetTop - 200, behavior: "smooth" });
   }
-}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pruebas/data.js
@@ -0,0 +1,14 @@
+const productos = {
+  1: {
+    nombre: "Producto 1",
+    precio: "S/ 29.00",
+    descripcion: "Descripción completa del producto 1",
+    img: "img/bebe...jpg"
+  },
+  2: {
+    nombre: "Producto 2",
+    precio: "S/ 45.00",
+    descripcion: "Descripción completa del producto 2",
+    img: "img/bebe...jpg"
+  }
+};
