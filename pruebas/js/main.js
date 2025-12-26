bb3352205035cb0abdaad7a1d02a021b66d7886c
eduardo <real.777.em@gmail.com>
2025-12-26 16:36:47 -0500

    dando detalles  ala version movil

--- pruebas/js/main.js
+++ pruebas/js/main.js
@@ -101,12 +101,82 @@ if (zoomBox && zoomImg) {
   });
 
   // MÓVIL - dedo
-  zoomBox.addEventListener('touchmove', e => {
-    const touch = e.touches[0];
-    const rect = zoomBox.getBoundingClientRect();
-    const x = ((touch.clientX - rect.left) / rect.width) * 100;
-    const y = ((touch.clientY - rect.top) / rect.height) * 100;
-    zoomImg.style.transformOrigin = `${x}% ${y}%`;
-  });
+  // zoomBox.addEventListener('touchmove', e => {
+  //   const touch = e.touches[0];
+  //   const rect = zoomBox.getBoundingClientRect();
+  //   const x = ((touch.clientX - rect.left) / rect.width) * 100;
+  //   const y = ((touch.clientY - rect.top) / rect.height) * 100;
+  //   zoomImg.style.transformOrigin = `${x}% ${y}%`;
+  // });
 
 }
+
+
+
+
+
+if (window.matchMedia('(max-width: 768px)').matches) {
+
+  let scale = 1;
+  let lastScale = 1;
+  let startDistance = 0;
+
+  let posX = 0;
+  let posY = 0;
+  let lastX = 0;
+  let lastY = 0;
+
+  // ⛔ desactiva lupa PC en móvil
+  zoomImg.style.transformOrigin = 'center center';
+
+  zoomBox.addEventListener('touchstart', e => {
+    if (e.touches.length === 2) {
+      startDistance = getDistance(e.touches[0], e.touches[1]);
+      lastScale = scale;
+    }
+
+    if (e.touches.length === 1) {
+      lastX = e.touches[0].clientX - posX;
+      lastY = e.touches[0].clientY - posY;
+    }
+  }, { passive: false });
+
+  zoomBox.addEventListener('touchmove', e => {
+    e.preventDefault();
+
+    // 🔍 Pinch (2 dedos)
+    if (e.touches.length === 2) {
+      const newDistance = getDistance(e.touches[0], e.touches[1]);
+      scale = Math.min(Math.max(lastScale * (newDistance / startDistance), 1), 4);
+    }
+
+    // 🧭 Pan (1 dedo)
+    if (e.touches.length === 1 && scale > 1) {
+      posX = e.touches[0].clientX - lastX;
+      posY = e.touches[0].clientY - lastY;
+    }
+
+    if (scale === 1) {
+      posX = 0;
+      posY = 0;
+    }
+
+    zoomImg.style.transform =
+      `translate(${posX}px, ${posY}px) scale(${scale})`;
+
+  }, { passive: false });
+
+  function getDistance(t1, t2) {
+    return Math.hypot(
+      t2.clientX - t1.clientX,
+      t2.clientY - t1.clientY
+    );
+  }
+}
+
+
+
+
+
+
+
